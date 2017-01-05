9267cd33963ef369fc68a6ce90b532bf329ecdc1
Unknown <marcelowarth@gmail.com>
2017-01-05 13:43:45 -0200

    Number wizard secao 2 aula 17

--- Number Wizard/Assets/Scripts/NumberWizard.cs
+++ Number Wizard/Assets/Scripts/NumberWizard.cs
@@ -11,10 +11,21 @@ public class NumberWizard : MonoBehaviour {
 		print("Pick a number in you head and dont tell me.");
 		print("The highest number you can pick is " + max + ".");
 		print("The lowest number you can pick is " + min + ".");
+		print("Is the number higher or lower than 500?");
+		print("Up arrow = higher, down arrow = lower, return = equal.");
+		
 	}
 	
 	// Update is called once per frame
 	void Update () {
-	
+		if(Input.GetKeyDown(KeyCode.UpArrow)) {
+			print("Up arrow pressed.");
+		}
+		if(Input.GetKeyDown(KeyCode.DownArrow)) {
+			print("Down arrow pressed.");
+		}
+		if(Input.GetKeyDown(KeyCode.Return)) {
+			print("Return pressed.");
+		}
 	}
 }

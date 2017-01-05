7bcd06d7a98612e0663b54ef5cc061253b718f88
Unknown <marcelowarth@gmail.com>
2017-01-05 14:22:03 -0200

    Number Wizard console concluido secao 2 aula 21

--- Number Wizard/Assets/Scripts/NumberWizard.cs
+++ Number Wizard/Assets/Scripts/NumberWizard.cs
@@ -2,11 +2,22 @@
 using System.Collections;
 
 public class NumberWizard : MonoBehaviour {
-	int max = 1000, min = 1, guess = 500;
+	int max, min, guess;
 	
 	// Use this for initialization
 	void Start () {
 		StartGame();
+	}
+	
+	void StartGame () {
+		ResetVariables();
+		print("========================");
+		print("Welcome to Number Wizard");
+		print("Pick a number in you head and dont tell me.");
+		print("The highest number you can pick is " + max + ".");
+		max = max + 1;
+		print("The lowest number you can pick is " + min + ".");
+		GuessMessage();
 	}
 	
 	// Update is called once per frame
@@ -19,16 +30,14 @@ public class NumberWizard : MonoBehaviour {
 			NextGuess();
 		} else if(Input.GetKeyDown(KeyCode.Return)) {
 			print("I won!");
+			StartGame();
 		}
 	}
 	
-	void StartGame () {
-		print("Welcome to Number Wizard");
-		print("Pick a number in you head and dont tell me.");
-		print("The highest number you can pick is " + max + ".");
-		max = max + 1;
-		print("The lowest number you can pick is " + min + ".");
-		GuessMessage();
+	void ResetVariables () {
+		max = 1000;
+		min = 1;
+		guess = 500;
 	}
 	
 	void NextGuess () {

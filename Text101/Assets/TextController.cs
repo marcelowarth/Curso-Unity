c9abc543b8033aaf011a5b60d7710fe5fe5633ea
Unknown <marcelowarth@gmail.com>
2017-01-06 10:32:09 -0200

    Text101 secao 3 aula 30

--- Text101/Assets/TextController.cs
+++ Text101/Assets/TextController.cs
@@ -14,7 +14,11 @@ public class TextController : MonoBehaviour {
 	// Update is called once per frame
 	void Update () {
 		if(Input.GetKeyDown(KeyCode.Space)){
-			text.text = "Space key pressed";
+			text.text = "You are in a prision cell, and you want to escape. There are " +
+				"some dirty sheets on the bed, a mirror on the wall and the door " +
+				"exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor " +
+				"is locked from the outside.\n\n" + 
+				"Press S to view Sheets, M to view Mirror and L to view Lock";
 		}
 	}
 }

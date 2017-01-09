05ce15a9bb6bd21a14fd39494d8e07f4f5546cda
Unknown <marcelowarth@gmail.com>
2017-01-09 09:28:36 -0200

    NumberWizardUI secao 4 aula 47

---
+++ NumberWizardUI/Assets/LevelManager.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LevelManager : MonoBehaviour {
+	
+	public void LoadLevel(string name) {
+		
+	}
+	
+	public void QuitRequest() {
+		
+	}
+	
+}

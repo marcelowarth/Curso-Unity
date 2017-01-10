73cb2b16942aaf327050d70a21b880fc9e0d700d
Unknown <marcelowarth@gmail.com>
2017-01-10 11:26:04 -0200

    NumberWizardUI secao 4 aula 50

---
+++ NumberWizardUI/Assets/NumberWizard.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class NumberWizard : MonoBehaviour {
+	int max, min, guess, maxGuessesAllowed = 5;
+	public Text text;
+	
+	void Start () {
+		StartGame();
+	}
+	
+	void StartGame () {
+		ResetVariables();
+		max = max + 1;
+	}
+		
+	public void GuessHigher() {
+		min = guess;
+		NextGuess();
+	}
+	
+	public void GuessLower() {
+		max = guess;
+		NextGuess();
+	}
+		
+	void ResetVariables () {
+		max = 1000;
+		min = 1;
+		guess = 500;
+	}
+	
+	void NextGuess () {
+		guess = (max + min) / 2;
+		text.text = guess.ToString();
+		maxGuessesAllowed = maxGuessesAllowed - 1;
+		if (maxGuessesAllowed <= 0) {
+			Application.LoadLevel("Win");
+		}
+	}
+	
+}

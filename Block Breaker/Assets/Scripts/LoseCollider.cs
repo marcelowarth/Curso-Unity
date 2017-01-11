235d7ffa06bf2f411c26f6e1484cdab0f0d36ba0
Unknown <marcelowarth@gmail.com>
2017-01-11 14:13:23 -0200

    Block Breaker secao 5 aula 70

---
+++ Block Breaker/Assets/Scripts/LoseCollider.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LoseCollider : MonoBehaviour {
+
+	void OnTriggerEnter2D(Collider2D collider) {
+		
+	}
+	
+	void OnCollisionEnter2D(Collision2D collision) {
+		
+	}
+}

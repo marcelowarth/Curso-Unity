b5ee2cb47818f2074c15743b901cc99f9997c282
Unknown <marcelowarth@gmail.com>
2017-01-11 10:19:08 -0200

    Block Breaker secao 5 aula 65

--- Block Breaker/Assets/Scripts/MusicPlayer.cs
+++ Block Breaker/Assets/Scripts/MusicPlayer.cs
@@ -3,9 +3,16 @@ using System.Collections;
 
 public class MusicPlayer : MonoBehaviour {
 
+	static MusicPlayer instance = null;
+
 	// Use this for initialization
 	void Start () {
-		GameObject.DontDestroyOnLoad(gameObject);
+		if(instance != null) {
+			Destroy(gameObject);
+		} else {
+			instance = this;
+			GameObject.DontDestroyOnLoad(gameObject);
+		}
 	}
 	
 	// Update is called once per frame

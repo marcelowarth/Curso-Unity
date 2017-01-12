03a33e70c0640568de5b22830c8b1a1b869b4806
Unknown <marcelowarth@gmail.com>
2017-01-12 09:24:23 -0200

    Block Breaker secao 5 aula 75

---
+++ Block Breaker/Assets/Scripts/Ball.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Ball : MonoBehaviour {
+	
+	public Paddle paddle;
+	public bool hasStarted = false;
+	
+	private Vector3 paddleToBallVector;
+	
+	// Use this for initialization
+	void Start () {
+		paddleToBallVector = this.transform.position - paddle.transform.position;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(!hasStarted) {
+			this.transform.position = paddle.transform.position + paddleToBallVector;
+		
+			if(Input.GetMouseButtonDown(0)) {
+				this.rigidbody2D.velocity = new Vector2(2f, 10f);
+				hasStarted = true;
+			}
+		}
+	}
+}

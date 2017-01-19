eeda270867f6153032954c6d73ee0f8d83f92fb8
Unknown <marcelowarth@gmail.com>
2017-01-19 11:15:52 -0200

    Laser Defender secao 6 aulas 102. 103. 104

---
+++ Laser Defender/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour {
+	
+	public float velocity = 15.0f;
+	public float padding = 1f;
+	
+	float xMin, xMax;
+	
+	// Use this for initialization
+	void Start () {
+		float distance = transform.position.z - Camera.main.transform.position.z;
+		Vector3 leftMost = Camera.main.ViewportToWorldPoint(new Vector3(0,0,distance));
+		Vector3 rightMost = Camera.main.ViewportToWorldPoint(new Vector3(1,0,distance));
+		xMin = leftMost.x + padding;
+		xMax = rightMost.x - padding;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(Input.GetKey(KeyCode.LeftArrow)) {
+			transform.position += Vector3.left * velocity * Time.deltaTime;
+		} else if(Input.GetKey(KeyCode.RightArrow)) {
+			transform.position += Vector3.right * velocity * Time.deltaTime;
+		}
+		
+		// Restrict to game space
+		float newX = Mathf.Clamp(transform.position.x, xMin, xMax);
+		transform.position = new Vector3(newX, transform.position.y, transform.position.z);
+	}
+}

9b087b26cb365d3711ea7abffb883f2c86724c71
Unknown <marcelowarth@gmail.com>
2017-01-25 10:48:29 -0200

    Glitch Garden secao 7 aulas 129, 130, 131

---
+++ Glitch Garden/Assets/Scripts/LevelManager.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LevelManager : MonoBehaviour {
+
+	public float loadNextLevelAfter = 2.5f;
+	
+	void Start () {
+		if(Application.loadedLevel == 0) {
+			Invoke ("LoadNextLevel", loadNextLevelAfter);
+		}
+	}
+	
+	public void LoadLevel(string name) {
+		Application.LoadLevel(name);
+	}
+	
+	public void QuitRequest() {
+		Application.Quit();
+	}
+	
+	public void LoadNextLevel() {
+		Application.LoadLevel(Application.loadedLevel + 1);
+	}
+}
---
+++ Glitch Garden/Assets/Scripts/MusicManager.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MusicManager : MonoBehaviour {
+
+	public AudioClip[] levelMusicChangeArray;
+	
+	private AudioSource audioSource;
+
+	void Awake() {
+		DontDestroyOnLoad(gameObject);
+	}
+
+	// Use this for initialization
+	void Start () {
+		audioSource = GetComponent<AudioSource>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+	
+	void OnLevelWasLoaded(int level) {
+		AudioClip thisLevelMusic = levelMusicChangeArray[level];
+		if(thisLevelMusic) {
+			audioSource.clip = thisLevelMusic;
+			audioSource.loop = true;
+			audioSource.Play();
+		}
+	}
+}

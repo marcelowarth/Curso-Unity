75f45af8ae88b60f6e8d460c37d2e3625aad2d10
Unknown <marcelowarth@gmail.com>
2017-01-26 09:54:50 -0200

    Glitch Garden secao 7 aulas 132, 133, 134

---
+++ Glitch Garden/Assets/Scripts/FadeIn.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class FadeIn : MonoBehaviour {
+
+	public float fadeTime;
+	
+	private Image fadePanel;
+	private Color currentColor = Color.black;
+
+	// Use this for initialization
+	void Start () {
+		fadePanel = GetComponent<Image>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(Time.timeSinceLevelLoad < fadeTime) {
+			float alphaChange = Time.deltaTime / fadeTime;
+			currentColor.a -= alphaChange;
+			fadePanel.color = currentColor;
+		} else {
+			gameObject.SetActive(false);
+		}
+	}
+}

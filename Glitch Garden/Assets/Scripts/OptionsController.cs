c0feee36aaf203bddf2f36bfa90cf5a56d0fce49
Unknown <marcelowarth@gmail.com>
2017-01-26 11:19:26 -0200

    Glitch Garden secao 7 aulas 136, 137, 138

---
+++ Glitch Garden/Assets/Scripts/OptionsController.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class OptionsController : MonoBehaviour {
+	
+	public Slider volumeSlider;
+	public Slider difficultySlider;
+	public LevelManager levelManager;
+	
+	private MusicManager musicManager;
+
+	// Use this for initialization
+	void Start () {
+		musicManager = GameObject.FindObjectOfType<MusicManager>();
+		volumeSlider.value = PlayerPrefsManager.GetMasterVolume();
+		difficultySlider.value = PlayerPrefsManager.GetDificilty();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		musicManager.ChangeVolume(volumeSlider.value);
+	}
+	
+	public void saveInExit() {
+		PlayerPrefsManager.SetMasterVolume(volumeSlider.value);
+		PlayerPrefsManager.SetDifficulty(difficultySlider.value);
+		levelManager.LoadLevel("01a Start");
+	}
+	
+	public void SetDefaults() {
+		volumeSlider.value = 1f;
+		difficultySlider.value = 2f;
+	}
+}

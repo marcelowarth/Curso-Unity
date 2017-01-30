588d89dd65ffad509a07355b1400bf5983775b00
Unknown <marcelowarth@gmail.com>
2017-01-30 10:12:45 -0200

    Glitch Garden secao 7 aulas 155, 156, 157

---
+++ Glitch Garden/Assets/Scripts/Attackers/Attacker.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+[RequireComponent(typeof (Rigidbody2D))]
+public class Attacker : MonoBehaviour {
+
+	private float currentSpeed;
+	private GameObject currectTarget;
+
+	// Use this for initialization
+	void Start () {
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		transform.Translate(Vector3.left * currentSpeed * Time.deltaTime);
+	}
+	
+	void OnTriggerEnter2D () {
+		
+	}
+	
+	public void SetSpeed(float speed) {
+		currentSpeed = speed;
+	}
+	
+	public void StrikeCurrentTarget(float damage) {
+		
+	}
+	
+	public void Attack(GameObject obj) {
+		currectTarget = obj;
+	}
+}
